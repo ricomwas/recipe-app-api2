580a1cc9a96e2fdbbc6f2b348f2e23f12043a231
Eric <ericmwangi82@gmail.com>
2023-11-17 01:23:46 +0300

    additional changes:
    - added some configs in the docker-compose yml file for the db
    
    - added some db installation configs in the dockerfile file
    
    - added a package to install in the requirements.txt file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Perform Calculator Functions
+"""
+
+def add(x, y):
+  """Add x and y return result"""
+  return x + y
+
+def substract(x, y):
+  """Substract x from y and return result"""
+  return y - x
