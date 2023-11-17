a949e39e9f076a5853d6a89a7c040ec5d47cc676
Eric <ericmwangi82@gmail.com>
2023-11-17 08:18:01 +0300

    fixing linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,14 +4,8 @@ Perform Calculator Functions
 
 
 def add(x, y):
-  """Add x and y return result"""
-
-  
   return x + y
 
 
 def substract(x, y):
-  """Substract x from y and return result"""
-
-
   return y - x
